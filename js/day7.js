4753e5b70b7f4e640dff1ee2b4bac983943d1b6d
alison-grow <agrowz98@gmail.com>
2022-07-21 19:01:16 -0700

    the final day7 answer

--- js/day7.js
+++ js/day7.js
@@ -1,27 +1,43 @@
 // DAY 7 IN-CLASS
 
 
-// function fizzbuzz(){
-//     for (let i = 1; i <= 100; i++) {
-//         if (i % 3 === 0 && i % 5 === 0) {
-//         console.log("FizzBuzz");
-//         } else if (i % 3 === 0) {
-//         console.log("Fizz");
-//         } else if (i % 5 === 0) {
-//         console.log("Buzz");
-//         } else {
-//         console.log(i);
-//         }
-//     }
-// }
+function fizzbuzz(){
+    for (let i = 1; i <= 100; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+        } else if (i % 3 === 0) {
+        console.log("Fizz");
+        } else if (i % 5 === 0) {
+        console.log("Buzz");
+        } else {
+        console.log(i);
+        }
+    }
+}
 
 
 // DAY 7 TAKE-HOME
 
-document.getElementById("catbtn").addEventListener("click", getRandomCatFact);
+const catFactsButton = document.createElement('button')
+catFactsButton.innerHTML = "🐈‍⬛ Get a random cat fact!!! 🐈‍⬛"
+catFactsButton.addEventListener('click', getRandomCatFact)
 
-async function getRandomCatFact() {
-    let x = await fetch('https://cat-fact.herokuapp.com'); 
-    let y = await x.Fact()
-    document.getElementById("myText").innerHTML = y;
+const BODY = document.querySelector('body')
+
+
+BODY.appendChild(catFactsButton)
+
+function getRandomCatFact() {
+    console.dir(document)
+    fetch('https://cat-fact.herokuapp.com/facts/random')
+    .then(res => {
+        console.log(res)
+        return res.json()
+    })
+    .then(data => {
+        console.log(data)
+        const catFact = document.createElement('p')
+        catFact.innerHTML = `Here is your cat fact :)... ${data.text}`
+        BODY.append(catFact)
+    })
 }
